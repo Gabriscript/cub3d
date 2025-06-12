e781ada3823edc52ad3204ed0fff828fe3ee5b5a
Claudio Borromei <cborrome@c1r5p17.hive.fi>
2025-06-12 13:02:17 +0300

    not solved last number error

--- src/01checks/color_check.c
+++ src/01checks/color_check.c
@@ -6,31 +6,93 @@
 /*   By: cborrome <cborrome@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 09:51:37 by cborrome          #+#    #+#             */
-/*   Updated: 2025/06/12 09:51:38 by cborrome         ###   ########.fr       */
+/*   Updated: 2025/06/12 13:01:20 by cborrome         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+static void	comma_check(char *path, int *x, int *comma_counter, t_game *game)
+{
+	int	y;
+	
+	if (path[*x] == ',')
+		{
+			(*comma_counter) += 1;
+			y = 1;
+			while (path[*x + y] == ' ')
+				y++;
+			if (path[*x + y] == ',')
+				simple_exit("Error\nInvalid color path\n", game);
+		}
+}
+
+static void	order_check(char *path, t_game *game)
+{
+	int	x;
+	int	number_counter;
+	int	comma_counter;
+	printf("[DEBUG] order_check path PRE %s\n", path);
+	x = 0;
+	number_counter = 0;
+	comma_counter = 0;
+	printf("[DEBUG] order_check path POST %s\n", path);
+	while (path[x])
+	{	
+		while (path[x] == ' ')
+			x++;
+		if (path[x] == ',' && number_counter == 0)
+			simple_exit("Error\nInvalid color path ORDER CHECK\n", game);
+		else if (path[x] == ',')
+		{
+			comma_counter += 1;
+			x++;
+			continue ;
+		}
+		else if (path[x] >= '0' || path[x] <= '9')
+		{
+			number_counter += 1;
+			x++;
+			continue ;
+		}
+		else if (comma_counter == 2)
+		{
+			while (path[x])
+			{
+				while (path[x] == ' ')
+					x++;
+				if (!path[x] || !((path[x] >= '0' && path[x] <= '9')))
+					simple_exit("Error\nInvalid color path END LINE\n", game);
+				x++;
+			}
+		}
+		
+		x++;
+	}
+}
 
 static void	color_path_check(char *path, t_game *game)
 {
 	int	x;
 	int	comma_counter;
 	int	number_counter;
-
+	printf("[DEBUG] in color_path_check %s\n", path);
 	x = 0;
 	comma_counter = 0;
 	number_counter = 0;
 	while (path[x])
 	{
-		if (path[x] == ',')
-			comma_counter += 1;
-		if (path[x] >= '0' || path[x] <= 9)
+		comma_check(path, &x, &comma_counter, game);
+		if (path[x] >= '0' && path[x] <= '9')
 			number_counter += 1;
+		else if (!((path[x] >= '0' && path[x] <= '9') || path[x] == ' '
+			|| path[x] == ','))
+			simple_exit("Error\nInvalid color path ELSE IF\n", game);
 		x++;
 	}
-	if (comma_counter > 2 || number_counter > 9)
-		simple_exit("Error\nInvalid color path\n", game);
+	if (comma_counter != 2 || number_counter < 3 || number_counter > 9)
+		simple_exit("Error\nInvalid color pathNUMBER\n", game);
+	order_check(path, game);
 }
 
 void	color_check(t_game *game)
